84527a72b896252bc417ebc93d67012fa6e52ed9
YaaY <ayoub.ezzini@youcan.shop>
2023-12-23 15:29:42 +0100

    updates

--- build.sh
+++ build.sh
@@ -3,5 +3,7 @@ echo 'PREPARING VESSEL...'
 cp ./.env ./laradock/.env
 cp nginx/sites/* ./laradock/nginx/sites/
 echo 'BUILDING DOCKER CONTAINERS...'
-cd ./laradock && docker-compose up -d nginx mysql workspace php-worker redis php-fpm mailhog certbot
+cd ./laradock && docker-compose up -d nginx mysql workspace php-worker redis php-fpm mailhog
+docker exec vessel-php-fpm-1 chown www-data:www-data /var/log
+docker exec vessel-php-fpm chown www-data:www-data /var/log
 echo 'VESSEL IS READY...'
